c28e46d818e8ebeed58f7c702f85a495326be6fc
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-07-28 10:30:46 +0200

    Command registry work

--- main.go
+++ main.go
@@ -8,20 +8,6 @@ import (
 	"strings"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-var commandRegistry = map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
-}
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Pokedex > ")
@@ -59,8 +45,34 @@ func cleanInput(text string) []string {
 	return wordsList
 }
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandRegistry = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+}
+
 func commandExit() error {
 	fmt.Printf("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
 	return nil
 }
